1c9fc0b2fbf20ac8a8797638afe5c62d4c3afefe
ushi-masuka <utkarshgupta2234@gmail.com>
2026-01-29 17:51:40 +0530

    stopped after encountering stateless issue.

--- main.py
+++ main.py
@@ -5,16 +5,24 @@ import streamlit as st
 
 
 def main():
+    client_list=[None ,'BakExcuse','PavWOW','Priya Creation','Sakhi Silai']
     print("Hello from streamlit-demo-social!")
 
     st.title('hello, bhaiyon!')
     st.subheader('ready for some new vids?')
-    st.text("lets make our clients and our wallets and our girlfriends proud")
-    st.write("please enter client info")
-    client=st.selectbox("please choose the client:",['BakExcuse','PavWOW','Priya Creation'])
-    st.write(f'great here is all info about {client}')
+    st.text("lets make our clients , wallets and girlfriends proud")
+    if st.button("add client"):
+        st.text("please enter client info")
+        new_client=st.text_input("Client Firm name: ")
+        client_list.append(new_client)
 
-    st.success(f"great! here is your entire {client} library!")
+        
+    
+    client=st.selectbox("please choose the client:", client_list)
+    
+    if client:
+        st.write(f'great here is all info about {client}')
+        st.success(f"great! here is your entire {client} library!")
 
 if __name__ == "__main__":
     main()
